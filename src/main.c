009337313823c8693cacc6d7c352208eaa1550e1
antonioasaro <antonioasaro@users.noreply.github.com>
2015-07-17 16:22:55 -0400

    Time to register app

--- src/main.c
+++ src/main.c
@@ -5,9 +5,12 @@ TextLayer *text_layer;
 
 void handle_init(void) {
   my_window = window_create();
+  window_stack_push(my_window, true);
 
   text_layer = text_layer_create(GRect(0, 0, 144, 20));
-  window_stack_push(my_window, true);
+  text_layer_set_text_color(text_layer, GColorBlack);	
+  text_layer_set_text(text_layer, "Subscribing ...");
+  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_layer));	
 }
 
 void handle_deinit(void) {
