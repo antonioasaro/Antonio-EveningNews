5518c8d2d6d996eae31a50556e25a945727fb972
antonioasaro <antonio_asaro@hotmail.com>
2015-07-18 15:02:17 -0400

    Change the filename

--- server/eveningnews.php
+++ server/eveningnews.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php   
-            require_once '../TimelineAPI/Timeline.php';
+            require_once './TimelineAPI/Timeline.php';
             use TimelineAPI\Pin;
             use TimelineAPI\PinLayout;
             use TimelineAPI\PinLayoutType;
