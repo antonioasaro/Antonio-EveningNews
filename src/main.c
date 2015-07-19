24e7a6e37fdf1d6cd402ad7a21ba583e76e43e17
antonioasaro <antonioasaro@users.noreply.github.com>
2015-07-19 12:44:24 -0400

    Re-adjusted text ...

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ void handle_init(void) {
   my_window = window_create();
   window_stack_push(my_window, true);
 
-  title_text_layer = text_layer_create(GRect(0, 44, 144, 40));
+  title_text_layer = text_layer_create(GRect(0, 50, 144, 40));
   text_layer_set_font(title_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(title_text_layer, GTextAlignmentCenter);
   text_layer_set_text_color(title_text_layer, GColorBlack);	
@@ -16,10 +16,10 @@ void handle_init(void) {
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(title_text_layer));	
 
   body_text_layer = text_layer_create(GRect(0, 80, 144, 40));
-  text_layer_set_font(body_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_font(body_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   text_layer_set_text_alignment(body_text_layer, GTextAlignmentCenter);
   text_layer_set_text_color(body_text_layer, GColorBlack);	
-  text_layer_set_text(body_text_layer, "Timeline pins coming ...");
+  text_layer_set_text(body_text_layer, "Timeline pin coming.");
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(body_text_layer));	
 }
 
