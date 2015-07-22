bba9c77cccd5f7dae1543c1b7db2716e7d0617b2
antonioasaro <antonioasaro@users.noreply.github.com>
2015-07-22 16:37:39 -0400

    Added status info to .js file

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,12 +1,28 @@
 Pebble.addEventListener("ready",
-   function(e) {
-      Pebble.timelineSubscribe('all-users',
-         function () {
-            console.log('Subscribed to all-users');
-         },
-         function (errorString) {
-            console.log('Error subscribing to topic: ' + errorString);
-         }
-     );
-   }
+	function(e) {
+		Pebble.getTimelineToken(
+  			function (token) {
+   				console.log('My timeline token is ' + token);
+  			},
+  			function (error) { 
+    			console.log('Error getting timeline token: ' + error);
+  			}
+		);  
+      	Pebble.timelineSubscribe('all-users',
+        	function () {
+            	console.log('Subscribing to all-users');
+         	},
+         	function (errorString) {
+            	console.log('Error subscribing to topic: ' + errorString);
+         	}
+     	);
+		Pebble.timelineSubscriptions(
+  			function (topics) {
+				console.log('Subscription list: ' + topics.join(', '));
+  			},
+  			function (errorString) {
+    			console.log('Error getting subscriptions: ' + errorString);
+  			}
+		);
+   	}
 );
