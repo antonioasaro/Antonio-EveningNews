3be68a9e2f9332b985499f3ad3b472cbc260e396
antonioasaro <antonio_asaro@hotmail.com>
2015-07-22 07:34:49 -0400

    Re-organize code

--- server/eveningnews.php
+++ server/eveningnews.php
@@ -20,40 +20,42 @@ and open the template in the editor.
             use TimelineAPI\Timeline;
 
             $mode = "create";
+            $body = "Will be posted at ~6:30pm ET"; 
             if (isset($_GET["mode"])) $mode = $_GET["mode"];
             
-            //Process RSS feed
-            $url = "https://news.google.com/?output=rss";
-            $xml = simplexml_load_file($url);
-            $allTheNews = "";
-            $search = array('&#39;', '&quot;');
-            $replace = array("'", '"');
-
-            for ($i=1; $i<=5; $i++) {
-                $description = strip_tags($xml->channel->item[$i]->description, '<br>');
-                $content = explode("<br>", $description);
-                $fixed = str_replace($search, $replace, $content[2]);
-                $allTheNews .= "$i: $fixed\n ";
-                echo "<b>$content[2]</b><br>$content[4]<br><br>"; 
-            }        
-
-            //Create and send pin
             $timezone = date_default_timezone_get();
             echo "The current server timezone is: " . $timezone . '<br>';                   
+            //Process RSS feed
+            if ($mode != "create") { 
+            	$url = "https://news.google.com/?output=rss";
+            	$xml = simplexml_load_file($url);
+            	$search = array('&#39;', '&quot;');
+            	$replace = array("'", '"');
+
+            	for ($i=1; $i<=5; $i++) {
+                	$description = strip_tags($xml->channel->item[$i]->description, '<br>');
+                	$content = explode("<br>", $description);
+                	$fixed = str_replace($search, $replace, $content[2]);
+                	$body .= "$i: $fixed\n ";
+                	echo "<b>$content[2]</b><br>$content[4]<br><br>"; 
+            	}        
+	    }
+
+            //Create and send pin
             $utc = new DateTimeZone('UTC');
             $amny = new DateTimeZone('America/New_York');
             
+	    $subtitle = date('F j') . " headlines";
             $newsTime = new DateTime('now', $amny);
             $newsTime->setTime(18, 30, 0);
             $newsTime->setTimeZone($utc);
-            if ($mode == "create") { $allTheNews = "Will be posted at ~6:30pm ET"; }
-            $pinlayout = new PinLayout(PinLayoutType::GENERIC_PIN, 'EveningNews', null, 'Top headlines', $allTheNews, PinIcon::NEWS_EVENT);            
+            $pinlayout = new PinLayout(PinLayoutType::GENERIC_PIN, 'EveningNews', null, $subtitle, $body, PinIcon::NEWS_EVENT);            
             $pin = new Pin('antonio-eveningnews-1', $newsTime, $pinlayout);
   
             $reminderTime = new DateTime('now', $amny);
             $reminderTime->setTime(18, 25, 0);
             $reminderTime->setTimeZone($utc);
-            $reminderlayout = new PinLayout(PinLayoutType::GENERIC_REMINDER, 'EveningNews reminder!!', null, 'stay tuned ...', null, PinIcon::NOTIFICATION_FLAG);
+            $reminderlayout = new PinLayout(PinLayoutType::GENERIC_REMINDER, 'EveningNews reminder!!', null, null, null, PinIcon::NOTIFICATION_FLAG);
             $reminder = new PinReminder($reminderlayout, $reminderTime);
             $pin -> addReminder($reminder);
             
@@ -62,7 +64,7 @@ and open the template in the editor.
             $topics = array('all-users');
             Timeline::pushSharedPin($apiKey, $topics, $pin);
             if ($mode == "create") { 
-                echo "Created shared pin for " . $newsTime->format('Y-m-d H:i:s') . " UTC<br>"; 
+                echo "Created shared pin set for " . $newsTime->format('Y-m-d H:i:s') . " UTC<br>"; 
             } else { 
                 echo "Updated shared pin.<br>";              
             }
